c6d21cce1a7da6792c75e90b1eae306c1ca8483e
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-07-13 16:12:38 +0300

    refactor: add prettier to server side

--- srv/onlinestore-service.js
+++ srv/onlinestore-service.js
@@ -1,16 +1,9 @@
-const cds = require("@sap/cds");
+const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(function () {
-  const {
-    Brands,
-    Categories,
-    Products,
-    SalesOrders,
-    SalesOrderItems,
-    Statuses,
-  } = this.entities();
+  const { Brands, Categories, Products, SalesOrders, SalesOrderItems, Statuses } = this.entities();
 
-  this.on("getProductRangeFilterParameters", async (req) => {
+  this.on('getProductRangeFilterParameters', async (req) => {
     const sProperty = req.data.property;
 
     const sValues = `{ MIN(${sProperty}) as min, MAX(${sProperty}) as max }`;
@@ -19,48 +12,47 @@ module.exports = cds.service.impl(function () {
     return await cds.run(oQuery);
   });
 
-  this.on("setDeliveredStatus", async (req) => {
+  this.on('setDeliveredStatus', async (req) => {
     const oParams = req.params[0];
     const oTarget = oParams.IsActiveEntity ? req.target : req.target.drafts;
 
     const oStatuses = await SELECT.from(Statuses)
-      .where({ title: ["Confirmed", "Delivered"] })
-      .columns(["ID", "title"]);
+      .where({ title: ['Confirmed', 'Delivered'] })
+      .columns(['ID', 'title'])
+      .orderBy(['title']);
 
-    const oStatusDelivered = oStatuses.find(
-      (oStatus) => oStatus.title === "Delivered"
-    );
-    const oStatusConfirmed = oStatuses.find(
-      (oStatus) => oStatus.title === "Confirmed"
-    );
+    const [oStatusConfirmed, oStatusDelivered] = oStatuses;
+    const [dbItemInfo] = await SELECT.from(oTarget).where({ ID: oParams.ID }).columns('status_ID');
 
-    await cds
-      .update(oTarget, oParams.ID)
-      .set({ status_ID: oStatusDelivered.ID })
-      .where({ status_ID: oStatusConfirmed.ID });
+    if (dbItemInfo.status_ID === oStatusConfirmed.ID) {
+      const tx = cds.transaction(req);
+      const affectedRows = await tx.run(
+        UPDATE(oTarget, oParams.ID).set({ status_ID: oStatusDelivered.ID }).where({ status_ID: oStatusConfirmed.ID })
+      );
+
+      if (affectedRows === 0) {
+        req.error(500, 'Sorry, something went wrong... Please try again later.');
+      }
+    }
   });
 
-  this.before(
-    "CREATE",
-    [Categories, Brands, Products, SalesOrders],
-    async (req) => {
-      await assignNewIdentifier(req.data, req.target);
-    }
-  );
+  this.before('CREATE', [Categories, Brands, Products, SalesOrders], async (req) => {
+    await assignNewIdentifier(req.data, req.target);
+  });
 
-  this.before("NEW", [SalesOrders], async (req) => {
+  this.before('NEW', [SalesOrders], async (req) => {
     await assignSalesOrderHeaderDefault(req.data, Statuses);
   });
 
-  this.before("PATCH", [SalesOrders], async (req) => {
+  this.before('PATCH', [SalesOrders], async (req) => {
     await setSalesOrderConfirmedStatus(req.data, Statuses);
   });
 
-  this.before(["NEW", "PATCH"], [SalesOrderItems], async (req) => {
+  this.before(['NEW', 'PATCH'], [SalesOrderItems], async (req) => {
     await recalculateSalesOrderItem(req.data, req.target, Products);
   });
 
-  this.after("PATCH", [SalesOrderItems], async (data, req) => {
+  this.after('PATCH', [SalesOrderItems], async (data, req) => {
     await recalculateSalesOrderTotals(data, req.target, SalesOrders);
   });
 });
@@ -72,52 +64,38 @@ async function assignNewIdentifier(data, target) {
 }
 
 async function assignSalesOrderHeaderDefault(data, Statuses) {
-  const oStatusNew = await SELECT.one.from(Statuses).where({ title: "New" });
+  const oStatusNew = await SELECT.one.from(Statuses).where({ title: 'New' });
   data.status_ID = oStatusNew.ID;
 }
 
 async function setSalesOrderConfirmedStatus(data, Statuses) {
   if (data.deliveryDate) {
-    const oStatusConfirmed = await SELECT.one
-      .from(Statuses)
-      .where({ title: "Confirmed" });
-
+    const oStatusConfirmed = await SELECT.one.from(Statuses).where({ title: 'Confirmed' });
     data.status_ID = oStatusConfirmed.ID;
   }
 }
 
 async function recalculateSalesOrderItem(data, target, Products) {
-  const dbItemInfo = await cds.read(target.drafts).where({ ID: data.ID });
+  const [dbItemInfo] = await cds.read(target.drafts).where({ ID: data.ID });
+  const oProduct = data.product_ID ? await SELECT.one.from(Products).where({ ID: data.product_ID }) : {};
 
-  const oProduct = data.product_ID
-    ? await SELECT.one.from(Products).where({ ID: data.product_ID })
-    : {};
+  data.quantity = data.quantity || dbItemInfo?.quantity || 1;
+  data.price = oProduct?.price || dbItemInfo?.price || 0;
 
-  data.quantity = data.quantity || dbItemInfo[0]?.quantity || 1;
-  data.price = oProduct?.price || dbItemInfo[0]?.price || 0;
-
-  data.currency_code = oProduct?.currency_code || dbItemInfo[0]?.currency_code;
+  data.currency_code = oProduct?.currency_code || dbItemInfo?.currency_code;
   data.amount = data.quantity * data.price;
 }
 
 async function recalculateSalesOrderTotals(data, target, SalesOrders) {
-  const orderInfo = { totalAmount: 0 };
+  const oOrderInfo = { totalAmount: 0 };
 
-  const result = await cds
-    .read(target.drafts)
-    .where({ ID: data.ID })
-    .columns(["parent_ID"]);
-
-  const sOrderID = result[0].parent_ID;
-
-  const dbItemInfos = await cds
-    .read(target.drafts)
-    .where({ parent_ID: sOrderID });
+  const [oItemData] = await cds.read(target.drafts).where({ ID: data.ID }).columns(['parent_ID']);
+  const dbItemInfos = await cds.read(target.drafts).where({ parent_ID: oItemData.ID });
 
   dbItemInfos.forEach((dbItemInfo) => {
-    orderInfo.totalAmount += dbItemInfo.amount;
-    orderInfo.currency_code = dbItemInfo.currency_code;
+    oOrderInfo.totalAmount += dbItemInfo.amount;
+    oOrderInfo.currency_code = dbItemInfo.currency_code;
   });
 
-  await cds.update(SalesOrders.drafts, sOrderID).set(orderInfo);
+  await cds.update(SalesOrders.drafts, sOrderID).set(oOrderInfo);
 }
